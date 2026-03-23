45cbe6933d4cd0d57baa8271fd09dc6c56db6460
teresa18181999 <teresalabomb@gmail.com>
2026-03-23 21:03:07 +0100

    n

--- b_vFjaW59To6f-1773949316363/app/(app)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { AppShell } from "@/components/app-shell"
-import { ReactNode } from "react"
-
-export default function FeedLayout({ children }: { children: ReactNode }) {
-  return <AppShell>{children}</AppShell>
-}
